2ae274f684d081a5eceb3ccafe0649bdbcc21d18
Xiao <xiao@scp-079.org>
2019-09-06 07:47:19 +0000

    - 0.0.1

---
+++ plugins/functions/image.py
@@ -0,0 +1,167 @@
+# SCP-079-NOSPAM - Block spam in groups
+# Copyright (C) 2019 SCP-079 <https://scp-079.org>
+#
+# This file is part of SCP-079-NOSPAM.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import logging
+import re
+
+from PIL import Image, ImageEnhance
+from pyrogram import Message
+from pytesseract import image_to_string
+from pyzbar.pyzbar import decode
+
+from .. import glovar
+
+# Enable logging
+logger = logging.getLogger(__name__)
+
+
+def get_color(path: str) -> bool:
+    # Get the picture's color, check if most of it is yellow
+    try:
+        image = Image.open(path).convert('YCbCr')
+        w, h = image.size
+        data = image.getdata()
+        cnt = 0
+        for i, ycbcr in enumerate(data):
+            y, cb, cr = ycbcr
+            if 86 <= cb <= 117 and 140 <= cr <= 168:
+                cnt += 1
+
+        if cnt > w * h * 0.3:
+            return True
+    except Exception as e:
+        logger.warning(f"Get color error: {e}", exc_info=True)
+
+    return False
+
+
+def get_file_id(message: Message) -> (str, bool):
+    # Get media message's image file id
+    file_id = ""
+    big = False
+    try:
+        if (message.photo
+                or (message.sticker and not message.sticker.is_animated)
+                or message.document
+                or message.game):
+            if message.photo:
+                file_id = message.photo.file_id
+            elif message.sticker:
+                file_id = message.sticker.file_id
+            elif message.document:
+                if (message.document.mime_type
+                        and "image" in message.document.mime_type
+                        and "gif" not in message.document.mime_type
+                        and message.document.file_size
+                        and message.document.file_size < glovar.image_size):
+                    file_id = message.document.file_id
+            elif message.game:
+                file_id = message.game.photo.file_id
+
+        if file_id:
+            big = True
+        elif ((message.animation and message.animation.thumbs)
+              or (message.audio and message.audio.thumbs)
+              or (message.video and message.video.thumbs)
+              or (message.video_note and message.video_note.thumbs)
+              or (message.document and message.document.thumbs)):
+            if message.animation:
+                file_id = message.animation.thumbs[-1].file_id
+            elif message.audio:
+                file_id = message.audio.thumbs[-1].file_id
+            elif message.video:
+                file_id = message.video.thumbs[-1].file_id
+            elif message.video_note:
+                file_id = message.video_note.thumbs[-1].file_id
+            elif message.document:
+                file_id = message.document.thumbs[-1].file_id
+    except Exception as e:
+        logger.warning(f"Get image status error: {e}", exc_info=True)
+
+    return file_id, big
+
+
+def get_ocr(path: str) -> str:
+    result = ""
+    try:
+        image = Image.open(path)
+        enhancer = ImageEnhance.Contrast(image)
+        image = enhancer.enhance(2)
+        result = image_to_string(image, lang='chi_sim+chi_tra')
+        if not result:
+            image = image.convert('L')
+            image = get_processed_image(image)
+            result = image_to_string(image, lang='chi_sim+chi_tra')
+
+        if result:
+            result = re.sub(r"\s{2,}", " ", result)
+    except Exception as e:
+        logger.warning(f"Get OCR error: {e}", exc_info=True)
+
+    return result
+
+
+def get_processed_image(image: Image.Image) -> Image.Image:
+    try:
+        image.thumbnail((200, 200))
+        s = 0
+        total = 0
+        for count, color in image.getcolors(image.size[0] * image.size[1]):
+            s += count * color
+            total += count
+
+        aver = int(s / total)
+        if aver < 110:
+            image = image.point(lambda x: 0 if x > aver + 20 else 255)
+        else:
+            image = image.point(lambda x: 0 if x < aver - 20 else 255)
+    except Exception as e:
+        logger.warning('Get image error: %s', e)
+
+    return image
+
+
+def get_qrcode(path: str) -> str:
+    # Get QR code
+    result = ""
+    try:
+        # Open
+        image = Image.open(path)
+
+        # Gray
+        image = image.convert("L")
+
+        # Contrast
+        image = ImageEnhance.Contrast(image).enhance(4.0)
+
+        # Thresholding
+        image = image.point(lambda x: 0 if x < 150 else 255)
+
+        # Decode
+        decoded_list = decode(image)
+        if decoded_list:
+            for decoded in decoded_list:
+                if decoded.type == "QRCODE":
+                    result += f"{decoded.data}\n"
+
+            if result:
+                result = result[:-1]
+    except Exception as e:
+        logger.warning(f"Get qrcode error: {e}", exc_info=True)
+
+    return result
---
+++ plugins/functions/telegram.py
@@ -0,0 +1,328 @@
+# SCP-079-NOSPAM - Block spam in groups
+# Copyright (C) 2019 SCP-079 <https://scp-079.org>
+#
+# This file is part of SCP-079-NOSPAM.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import logging
+from typing import Iterable, List, Optional, Union
+
+from pyrogram import Chat, ChatMember, Client, InlineKeyboardMarkup, Message
+from pyrogram.api.types import InputPeerUser, InputPeerChannel
+from pyrogram.errors import ChannelInvalid, ChannelPrivate, FloodWait, PeerIdInvalid
+from pyrogram.errors import UsernameInvalid, UsernameNotOccupied, UserNotParticipant
+
+from .. import glovar
+from .etc import delay, get_int, wait_flood
+
+# Enable logging
+logger = logging.getLogger(__name__)
+
+
+def delete_messages(client: Client, cid: int, mids: Iterable[int]) -> Optional[bool]:
+    # Delete some messages
+    result = None
+    try:
+        mids = list(mids)
+        mids_list = [mids[i:i + 100] for i in range(0, len(mids), 100)]
+        for mids in mids_list:
+            try:
+                flood_wait = True
+                while flood_wait:
+                    flood_wait = False
+                    try:
+                        result = client.delete_messages(chat_id=cid, message_ids=mids)
+                    except FloodWait as e:
+                        flood_wait = True
+                        wait_flood(e)
+            except Exception as e:
+                logger.warning(f"Delete message in {cid} for loop error: {e}", exc_info=True)
+    except Exception as e:
+        logger.warning(f"Delete messages in {cid} error: {e}", exc_info=True)
+
+    return result
+
+
+def download_media(client: Client, file_id: str, file_path: str):
+    # Download a media file
+    result = None
+    try:
+        flood_wait = True
+        while flood_wait:
+            flood_wait = False
+            try:
+                result = client.download_media(message=file_id, file_name=file_path)
+            except FloodWait as e:
+                flood_wait = True
+                wait_flood(e)
+    except Exception as e:
+        logger.warning(f"Download media {file_id} to {file_path} error: {e}", exc_info=True)
+
+    return result
+
+
+def get_admins(client: Client, cid: int) -> Optional[Union[bool, List[ChatMember]]]:
+    # Get a group's admins
+    result = None
+    try:
+        flood_wait = True
+        while flood_wait:
+            flood_wait = False
+            try:
+                result = client.get_chat_members(chat_id=cid, filter="administrators")
+            except FloodWait as e:
+                flood_wait = True
+                wait_flood(e)
+            except (PeerIdInvalid, ChannelInvalid, ChannelPrivate):
+                return False
+    except Exception as e:
+        logger.warning(f"Get admins in {cid} error: {e}", exc_info=True)
+
+    return result
+
+
+def get_chat_member(client: Client, cid: int, uid: int) -> Optional[ChatMember]:
+    # Get information about one member of a chat
+    result = None
+    try:
+        flood_wait = True
+        while flood_wait:
+            flood_wait = False
+            try:
+                result = client.get_chat_member(chat_id=cid, user_id=uid)
+            except FloodWait as e:
+                flood_wait = True
+                wait_flood(e)
+            except UserNotParticipant:
+                result = False
+    except Exception as e:
+        logger.warning(f"Get chat member error: {e}", exc_info=True)
+
+    return result
+
+
+def get_group_info(client: Client, chat: Union[int, Chat]) -> (str, str):
+    # Get a group's name and link
+    group_name = "Unknown Group"
+    group_link = glovar.default_group_link
+    try:
+        if isinstance(chat, int):
+            result = None
+            flood_wait = True
+            while flood_wait:
+                flood_wait = False
+                try:
+                    result = client.get_chat(chat_id=chat)
+                except FloodWait as e:
+                    flood_wait = True
+                    wait_flood(e)
+                except Exception as e:
+                    logger.info(f"Get chat {chat} error: {e}", exc_info=True)
+
+            chat = result
+
+        if chat.title:
+            group_name = chat.title
+
+        if chat.username:
+            group_link = "https://t.me/" + chat.username
+    except Exception as e:
+        logger.info(f"Get group info error: {e}", exc_info=True)
+
+    return group_name, group_link
+
+
+def get_messages(client: Client, cid: int, mids: Iterable[int]) -> Optional[List[Message]]:
+    # Get some messages
+    result = None
+    try:
+        flood_wait = True
+        while flood_wait:
+            flood_wait = False
+            try:
+                result = client.get_messages(chat_id=cid, message_ids=mids)
+            except FloodWait as e:
+                flood_wait = True
+                wait_flood(e)
+    except Exception as e:
+        logger.warning(f"Get messages error: {e}", exc_info=True)
+
+    return result
+
+
+def kick_chat_member(client: Client, cid: int, uid: Union[int, str]) -> Optional[Union[bool, Message]]:
+    # Kick a chat member in a group
+    result = None
+    try:
+        flood_wait = True
+        while flood_wait:
+            flood_wait = False
+            try:
+                result = client.kick_chat_member(chat_id=cid, user_id=uid)
+            except FloodWait as e:
+                flood_wait = True
+                wait_flood(e)
+    except Exception as e:
+        logger.warning(f"Kick chat member {uid} in {cid} error: {e}", exc_info=True)
+
+    return result
+
+
+def leave_chat(client: Client, cid: int) -> bool:
+    # Leave a channel
+    try:
+        flood_wait = True
+        while flood_wait:
+            flood_wait = False
+            try:
+                client.leave_chat(chat_id=cid)
+            except FloodWait as e:
+                flood_wait = True
+                wait_flood(e)
+
+        return True
+    except Exception as e:
+        logger.warning(f"Leave chat {cid} error: {e}", exc_info=True)
+
+    return False
+
+
+def resolve_peer(client: Client, pid: Union[int, str]) -> Optional[Union[bool, InputPeerChannel, InputPeerUser]]:
+    # Get an input peer by id
+    result = None
+    try:
+        flood_wait = True
+        while flood_wait:
+            flood_wait = False
+            try:
+                result = client.resolve_peer(pid)
+            except FloodWait as e:
+                flood_wait = True
+                wait_flood(e)
+            except (PeerIdInvalid, UsernameInvalid, UsernameNotOccupied):
+                return False
+    except Exception as e:
+        logger.warning(f"Resolve peer error: {e}", exc_info=True)
+
+    return result
+
+
+def resolve_username(client: Client, username: str) -> (str, int):
+    # Resolve peer by username
+    peer_type = ""
+    peer_id = 0
+    try:
+        if username:
+            result = resolve_peer(client, username)
+            if result:
+                if isinstance(result, InputPeerChannel):
+                    peer_type = "channel"
+                    peer_id = result.channel_id
+                    peer_id = get_int(f"-100{peer_id}")
+                elif isinstance(result, InputPeerUser):
+                    peer_type = "user"
+                    peer_id = result.user_id
+    except Exception as e:
+        logger.warning(f"Resolve username error: {e}", exc_info=True)
+
+    return peer_type, peer_id
+
+
+def send_document(client: Client, cid: int, file: str, text: str = None, mid: int = None,
+                  markup: InlineKeyboardMarkup = None) -> Optional[Union[bool, Message]]:
+    # Send a document to a chat
+    result = None
+    try:
+        flood_wait = True
+        while flood_wait:
+            flood_wait = False
+            try:
+                result = client.send_document(
+                    chat_id=cid,
+                    document=file,
+                    caption=text,
+                    parse_mode="html",
+                    reply_to_message_id=mid,
+                    reply_markup=markup
+                )
+            except FloodWait as e:
+                flood_wait = True
+                wait_flood(e)
+            except (PeerIdInvalid, ChannelInvalid, ChannelPrivate):
+                return False
+    except Exception as e:
+        logger.warning(f"Send document to {cid} error: {e}", exec_info=True)
+
+    return result
+
+
+def send_message(client: Client, cid: int, text: str, mid: int = None,
+                 markup: InlineKeyboardMarkup = None) -> Optional[Union[bool, Message]]:
+    # Send a message to a chat
+    result = None
+    try:
+        if text.strip():
+            flood_wait = True
+            while flood_wait:
+                flood_wait = False
+                try:
+                    result = client.send_message(
+                        chat_id=cid,
+                        text=text,
+                        parse_mode="html",
+                        disable_web_page_preview=True,
+                        reply_to_message_id=mid,
+                        reply_markup=markup
+                    )
+                except FloodWait as e:
+                    flood_wait = True
+                    wait_flood(e)
+                except (PeerIdInvalid, ChannelInvalid, ChannelPrivate):
+                    return False
+    except Exception as e:
+        logger.warning(f"Send message to {cid} error: {e}", exc_info=True)
+
+    return result
+
+
+def send_report_message(secs: int, client: Client, cid: int, text: str, mid: int = None,
+                        markup: InlineKeyboardMarkup = None) -> Optional[Message]:
+    # Send a message that will be auto deleted to a chat
+    result = None
+    try:
+        if text.strip():
+            flood_wait = True
+            while flood_wait:
+                flood_wait = False
+                try:
+                    result = client.send_message(
+                        chat_id=cid,
+                        text=text,
+                        parse_mode="html",
+                        disable_web_page_preview=True,
+                        reply_to_message_id=mid,
+                        reply_markup=markup
+                    )
+                except FloodWait as e:
+                    flood_wait = True
+                    wait_flood(e)
+
+            mid = result.message_id
+            mids = [mid]
+            delay(secs, delete_messages, [client, cid, mids])
+    except Exception as e:
+        logger.warning(f"Send message to {cid} error: {e}", exc_info=True)
+
+    return result
