2216d60d15914ad5165fc4d2954967547f845dbf
Xiao <xiao@scp-079.org>
2019-09-06 05:18:55 +0000

    - 0.0.1

---
+++ README.md
@@ -0,0 +1,52 @@
+# SCP-079-NOSPAM
+
+This bot is used to block spam in groups.
+
+## How to use
+
+See [this article](https://scp-079.org/nospam/).
+
+## To Do List
+
+- [ ] Basic functions
+
+## Requirements
+
+- Python 3.6 or higher
+- Ubuntu: `sudo apt update && sudo apt install libzbar0 opencc tesseract-ocr tesseract-ocr-chi-sim tesseract-ocr-chi-tra -y`
+- pip: `pip install -r requirements.txt` or `pip install -U APScheduler OpenCC Pillow pyAesCrypt pyrogram[fast] pytesseract pyzbar`
+
+## Files
+
+- plugins
+    - functions
+        - `channel.py` : Functions about channel
+        - `etc.py` : Miscellaneous
+        - `file.py` : Save files
+        - `filters.py` : Some filters
+        - `group.py` : Functions about group
+        - `ids.py` : Modify id lists
+        - `image.py` : Functions about image
+        - `receive.py` : Receive data from exchange channel
+        - `telegram.py` : Some telegram functions
+        - `tests.py` : Some test functions
+        - `timers.py` : Timer functions
+        - `user.py` : Functions about user and channel object
+    - handlers
+        - `command.py` : Handle commands
+        - `message.py`: Handle messages
+    - `glovar.py` : Global variables
+- `.gitignore` : Ignore
+- `config.ini.example` -> `config.ini` : Configuration
+- `LICENSE` : GPLv3
+- `main.py` : Start here
+- `README.md` : This file
+- `requirements.txt` : Managed by pip
+
+## Contribute
+
+Welcome to make this project even better. You can submit merge requests, or report issues.
+
+## License
+
+Licensed under the terms of the [GNU General Public License v3](LICENSE).
---
+++ main.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# SCP-079-NOSPAM - Block spam in groups
+# Copyright (C) 2019 SCP-079 <https://scp-079.org>
+#
+# This file is part of SCP-079-NOSPAM.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import logging
+
+from apscheduler.schedulers.background import BackgroundScheduler
+from pyrogram import Client
+
+from plugins import glovar
+from plugins.functions.timers import backup_files, interval_hour_01, interval_min_15
+from plugins.functions.timers import reset_data, send_count, update_admins, update_status
+
+# Enable logging
+logger = logging.getLogger(__name__)
+
+# Start
+app = Client(
+    session_name="bot",
+    bot_token=glovar.bot_token
+)
+app.start()
+
+# Timer
+scheduler = BackgroundScheduler()
+scheduler.add_job(interval_hour_01, "interval", [app], hours=1)
+scheduler.add_job(interval_min_15, "interval", minutes=15)
+scheduler.add_job(update_status, "cron", [app], minute=30)
+scheduler.add_job(backup_files, "cron", [app], hour=20)
+scheduler.add_job(send_count, "cron", [app], hour=21)
+scheduler.add_job(reset_data, "cron", day=glovar.reset_day, hour=22)
+scheduler.add_job(update_admins, "cron", [app], hour=22, minute=30)
+scheduler.start()
+
+# Run
+app.run()
