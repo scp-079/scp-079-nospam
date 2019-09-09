2036368facff7b6b1f6a4b3d7c5f61f1599656e7
Xiao <xiao@scp-079.org>
2019-09-09 13:38:52 +0000

    - 0.0.2

--- plugins/glovar.py
+++ plugins/glovar.py
@@ -169,7 +169,6 @@ project_link: str = ""
 project_name: str = ""
 punish_time: int = 0
 reset_day: str = ""
-time_ban: int = 0
 time_new: int = 0
 
 # [encrypt]
@@ -209,7 +208,6 @@ try:
     project_name = config["custom"].get("project_name", project_name)
     punish_time = int(config["custom"].get("punish_time", punish_time))
     reset_day = config["custom"].get("reset_day", reset_day)
-    time_ban = int(config["custom"].get("time_ban", time_ban))
     time_new = int(config["custom"].get("time_new", time_new))
     # [encrypt]
     key = config["encrypt"].get("key", key)
@@ -245,7 +243,6 @@ if (bot_token in {"", "[DATA EXPUNGED]"}
         or project_name in {"", "[DATA EXPUNGED]"}
         or punish_time == 0
         or reset_day in {"", "[DATA EXPUNGED]"}
-        or time_ban == 0
         or time_new == 0
         or key in {b"", b"[DATA EXPUNGED]"}
         or password in {"", "[DATA EXPUNGED]"}):
