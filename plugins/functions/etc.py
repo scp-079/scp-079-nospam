619c8e4e4de89b46c5995fd95f41aebca41c0975
Xiao <xiao@scp-079.org>
2019-09-14 03:32:52 +0000

    - 0.0.3

--- plugins/functions/etc.py
+++ plugins/functions/etc.py
@@ -410,7 +410,7 @@ def get_stripped_link(link: str) -> str:
 
 
 def get_text(message: Message) -> str:
-    # Get message's text, including link and mentioned user's name
+    # Get message's text, including links and buttons
     text = ""
     try:
         the_text = message.text or message.caption
