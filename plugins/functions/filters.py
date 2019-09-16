5472f7d2e60333b5ab695f4f887d5972bec04b8c
Xiao <xiao@scp-079.org>
2019-09-16 05:37:05 +0000

    - 0.0.4

--- plugins/functions/filters.py
+++ plugins/functions/filters.py
@@ -267,12 +267,12 @@ def is_bad_message(client: Client, message: Message, text: str = None, image_pat
             # Bypass
             message_text = get_text(message)
             description = get_description(client, gid)
-            if description and message_text == description:
+            if description and message_text in description:
                 return ""
 
             pinned_message = get_pinned(client, gid)
             pinned_text = get_text(pinned_message)
-            if pinned_text and message_text == pinned_text:
+            if pinned_text and message_text in pinned_text:
                 return ""
 
             group_sticker = get_group_sticker(client, gid)
