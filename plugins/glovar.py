ba2da3c0594b766ffd23c95b79a1a4cb9c13f7b1
Xiao <xiao@scp-079.org>
2020-11-02 09:36:32 +0000

    update

--- plugins/glovar.py
+++ plugins/glovar.py
@@ -91,7 +91,7 @@ zh_cn: Union[bool, str] = ""
 emoji_ad_single: int = 0
 emoji_ad_total: int = 0
 emoji_many: int = 0
-emoji_protect: str = ""
+emoji_protect: [bytes, str] = "\\U0001F642"
 emoji_wb_single: int = 0
 emoji_wb_total: int = 0
 
@@ -157,7 +157,8 @@ try:
     emoji_ad_single = int(config["emoji"].get("emoji_ad_single", str(emoji_ad_single)))
     emoji_ad_total = int(config["emoji"].get("emoji_ad_total", str(emoji_ad_total)))
     emoji_many = int(config["emoji"].get("emoji_many", str(emoji_many)))
-    emoji_protect = getdecoder("unicode_escape")(config["emoji"].get("emoji_protect", str(emoji_protect)))[0]
+    emoji_protect = config.get("emoji", "emoji_protect", fallback=emoji_protect).encode()
+    emoji_protect = getdecoder("unicode_escape")(emoji_protect)[0]
     emoji_wb_single = int(config["emoji"].get("emoji_wb_single", str(emoji_wb_single)))
     emoji_wb_total = int(config["emoji"].get("emoji_wb_total", str(emoji_wb_total)))
 
